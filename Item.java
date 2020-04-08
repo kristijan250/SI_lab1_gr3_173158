a7a9c14257616119cdb11113912bf6979c39c043
kristijan250 <63318471+kristijan250@users.noreply.github.com>
2020-04-08 21:30:25 +0200

    tax type variable added

--- Item.java
+++ Item.java
@@ -2,7 +2,12 @@ class Item {
 	int id;
 	String name;
 	double price;
-
+	char tip;
+	int Danok() {
+		if tip=="A" return 18;
+		if tip=="B" return 5;
+		if tip=="V" return 0;
+	}
 	//TODO add variable.
 
 	//TODO constructor
