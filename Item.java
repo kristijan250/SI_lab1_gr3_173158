cdb8aee6fb4e8577f9cd6b5a9d8d37f727485140
kristijan250 <63318471+kristijan250@users.noreply.github.com>
2020-04-08 21:43:57 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -8,13 +8,33 @@ class Item {
 		if tip=="B" return 5;
 		if tip=="V" return 0;
 	}
-	//TODO add variable.
-
 	//TODO constructor
-
+	Item(int id, double price, char tip){
+	    	this->id = id;
+	    	this->price = price;
+	    	strcpy(this->tip,tip);
+	}
 	//TODO setters and getters
-
+	void setid (int k) {   
+        id = k;   
+    }
+	void setname (char k) {   
+       strcpy(k,name);   
+	}
+	void setp (double k) {   
+        price = k;   
+    }
+	int getID() {
+		return id;
+	}
+	int getName() {
+		return name;
+	}
+	double getprice() {
+		return price;
+	}
 	double taxReturn () {
+		cout<<getID()<<getName()<<getprice()*0.15<<endl;
 		//TODO
 	}
 }
